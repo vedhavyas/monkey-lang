4bd4dd6ec34e2debd2f44143d78f2d10e7cfe68b
Vedhavyas Singareddi <vedhavyas.singareddi@gmail.com>
2017-04-19 07:12:09 +0530

    add simple lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,102 @@
+package lexer
+
+import "github.com/vedhavyas/monkey-lang/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	l.skipWhiteSpace()
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readChars(isLetter)
+			tok.Type = token.GetIdentifierType(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readChars(isDigit)
+			tok.Type = token.INT
+			return tok
+		}
+
+		tok = newToken(token.ILLEGAL, l.ch)
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tt token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tt, Literal: string(ch)}
+}
+
+func (l *Lexer) readChars(decider func(byte) bool) string {
+	position := l.position
+	for decider(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == '\n' || l.ch == '\t' || l.ch == '\r' || l.ch == ' ' {
+		l.readChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_' {
+		return true
+	}
+
+	return false
+}
+
+func isDigit(ch byte) bool {
+	if ch >= '0' && ch <= '9' {
+		return true
+	}
+
+	return false
+}
